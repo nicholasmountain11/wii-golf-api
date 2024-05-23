ad36ef23d995e8009038164d4b9de2d30321916c
nicholasmountain11 <nmountain@unc.edu>
2024-05-23 15:41:11 -0400

    Initial commit

---
+++ src/main/java/com/example/nickspring/NickspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.nickspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NickspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NickspringApplication.class, args);
+	}
+
+}
