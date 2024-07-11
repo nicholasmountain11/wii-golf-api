4cf10f3a10b6e2a622adc6aeeff49104159da44f
nicholasmountain11 <nmountain@unc.edu>
2024-07-11 14:56:29 -0400

    change to java 17 and update cross origins

--- src/main/java/com/example/nickspring/restservice/controller/GolfController.java
+++ src/main/java/com/example/nickspring/restservice/controller/GolfController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Set;
 
+@CrossOrigin
 @RestController
 public class GolfController {
     private final GolfService golfService;
